d8eac599b2be736b2610b91bcb43e9e4380ee112
oldjack <1316567457@qq.com>
2018-12-05 14:43:17 +0800

    第一次commit

--- src/main/java/com/t163/eshop/controller/test.java
+++ src/main/java/com/t163/eshop/controller/test.java
@@ -1,15 +1,23 @@
 package com.t163.eshop.controller;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class test {
+	
 	@RequestMapping("test.htm")
 	public String a(Model model) {
+		
 		model.addAttribute("num",20);
 		return "newFtl";
+	
+	
 	}
 	
 	@RequestMapping("test2.htm")
