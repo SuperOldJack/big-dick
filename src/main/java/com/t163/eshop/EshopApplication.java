fb9a16775c604d36f66b0f21f017557a7befb6fc
周杰 <1316567457@qq.com>
2018-12-08 14:22:44 +0800

    测试提交

--- src/main/java/com/t163/eshop/EshopApplication.java
+++ src/main/java/com/t163/eshop/EshopApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EshopApplication {
 
 	public static void main(String[] args) {
-		System.out.println("琪琪大傻吊");
+		System.out.println("琪琪大吊");
 		SpringApplication.run(EshopApplication.class, args);
 	}
 }
